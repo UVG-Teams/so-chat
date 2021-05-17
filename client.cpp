7857214d6b5863e0a6688009356847f745bc3f69
FranciscoRosal <ros18676@uvg.edu.gt>
2021-05-17 16:37:36 -0600

    fix

--- client.cpp
+++ client.cpp
@@ -164,7 +164,7 @@ int main(int argc, char *argv[]) {
                 // Salir
                 // ==================================
                 interrupt_handler(-1);
-                exit(0);
+                exit(1);
             default:
                 break;
         }
